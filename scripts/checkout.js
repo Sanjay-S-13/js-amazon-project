33270f070d095f9f60242b942e6235359aebcbfa
Sanjay-S-13 <sanjays.workspace@gmail.com>
2025-07-18 13:39:49 +0530

    delete products based on HTML when clicks

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,9 @@ cart.forEach((CartItem) => {
     });
     
 
-    cartSummaryHtml+=`<div class="cart-item-container">
+    cartSummaryHtml+=`
+    <div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -100,5 +102,10 @@ document.querySelectorAll('.js-delete-link')
         link.addEventListener('click' , () =>{
             const productId = link.dataset.productId;
             removeFromCart(productId);
+
+            const container = document.querySelector(
+                `.js-cart-item-container-${productId}`);
+                
+                container.remove();
         });
     });
