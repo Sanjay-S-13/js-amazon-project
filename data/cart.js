f40d5bdaa3cc37431c8fdf3a4da872aef0305046
Sanjay-S-13 <sanjays.workspace@gmail.com>
2025-07-18 12:39:30 +0530

    Generate Html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];//modules works only on live server
+export const cart = [{
+  ProductId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity : 2,
+},{
+  ProductId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity : 1,
+}];
+//modules works only on live server
 
 
   //checking whether cart is already contains a product 
