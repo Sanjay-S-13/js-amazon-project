898917d91e3b976432ddf498d5d256d45333985d
Sanjay-S-13 <sanjays.workspace@gmail.com>
2025-07-19 20:53:15 +0530

    Create Automated tests for AddtoCart()

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,10 +1,9 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 describe('Test suite : formatCurrency' , () => {
     it('converts cents into dollars' , () => {
         expect(formatCurrency(2095)).toEqual('20.95');
     });
-
 
     it('works with 0' , () => {
         expect(formatCurrency(0)).toEqual('0.00');
